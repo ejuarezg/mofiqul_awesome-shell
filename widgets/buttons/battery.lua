ejuarezg/mofiqul_awesome-shell

commit 0bf31439ad14236e3e2e01ff7005bc040af332a6

Add a workaround for an Awesome WM startup error

widgets/buttons/battery.lua
@@ -97,7 +97,7 @@ if stdout:match("Battery detected") then
 	}
 else
 	label:set_text("AC")
-	icon.image(beautiful.icon_ac)
+	-- icon.image(beautiful.icon_ac)
 	icon:emit_signal("widget::redraw_needed")
 	battery_panel_indicator:set_image(beautiful.icon_ac)
 end

commit 48d24d9cca655578b362e9f15080ab155057f3e7

Fix startup error caused by bad code in AC icon
This fixes the workaround introduced in 0bf31439ad14236e3e2e01ff7005bc040af332a6

widgets/buttons/battery.lua
@@ -97,7 +97,7 @@ if stdout:match("Battery detected") then
 	}
 else
 	label:set_text("AC")
-	-- icon.image(beautiful.icon_ac)
+	icon.image = beautiful.icon_ac
 	icon:emit_signal("widget::redraw_needed")
 	battery_panel_indicator:set_image(beautiful.icon_ac)
 end