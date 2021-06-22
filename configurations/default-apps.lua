a526c69f750275bc872f0f064e8f23e6add2982d
Ezequiel Juarez Garcia <ezequieljuarezg@gmail.com>
2021-06-22 11:51:25 -0400

    Fix typo in bluetooth manager command

--- configurations/default-apps.lua
+++ configurations/default-apps.lua
@@ -6,7 +6,7 @@ local default_apps = {}
 default_apps.screeh_shot = "spectacle"
 default_apps.lock_screen = config_dir .. "scripts/i3lock-blur "
 default_apps.software_updater = terminal .. " --title 'System upgrade' -e sudo pacman -Syu"
-default_apps.bluetooth_manager = terminal .. "-e bluetoothctl"
+default_apps.bluetooth_manager = terminal .. " -e bluetoothctl"
 default_apps.network_manager = terminal .. " -e nmtui"
 default_apps.app_menu = "rofi -dpi " .. screen.primary.dpi ..
 						" -show drun -theme " .. config_dir ..
