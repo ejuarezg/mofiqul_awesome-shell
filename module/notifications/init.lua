d2367e8405e6e62be94448282dab4adba161ff78
Ezequiel Juarez Garcia <ezequieljuarezg@gmail.com>
2021-06-24 09:52:51 -0400

    Add a helper function and require volume notifications

--- module/notifications/init.lua
+++ module/notifications/init.lua
@@ -7,6 +7,8 @@ local gears =  require("gears")
 local menubar = require("menubar")
 local clickable_container = require("widgets.clickable-container")
 
+local notifications = {}
+
 
 naughty.connect_signal(
 	'request::icon',
@@ -198,4 +200,33 @@ naughty.connect_signal("request::display", function (n)
 
 end)
 
+-- >> Notify DWIM (Do What I Mean):
+-- Create or update notification automagically. Requires storing the
+-- notification in a variable.
+--
+-- Example usage:
+--     local my_notif = notifications.notify_dwim({ title = "hello", message = "there" }, my_notif)
+--     -- After a while, use this to update or recreate the notification if it is expired / dismissed
+--     my_notif = notifications.notify_dwim({ title = "good", message = "bye" }, my_notif)
+--
+-- This function was nabbed from
+-- https://github.com/elenapan/dotfiles/blob/master/config/awesome/notifications/init.lua
+function notifications.notify_dwim(args, notif)
+    local n = notif
+    if n and not n._private.is_destroyed and not n.is_expired then
+        notif.title = args.title or notif.title
+        notif.message = args.message or notif.message
+        -- notif.text = args.text or notif.text
+        notif.icon = args.icon or notif.icon
+        notif.timeout = args.timeout or notif.timeout
+    else
+        n = naughty.notification(args)
+    end
+    return n
+end
 
+function notifications.init()
+    require("module.notifications.volume")
+end
+
+return notifications
